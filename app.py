db66145a1bbd367a16c9414f4587d50a76b1ebdc
Murillospurio <murillospuriopiraju@gmail.com>
2025-10-12 20:49:17 -0300

    atualizar app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from threading import Timer
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -80,5 +81,6 @@ def gerar_pagamento():
 def home():
     return "Servidor Flask online e pronto!", 200
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
